925296fcc3cf703a9d162f26bffed48cd171e10e
TS227 <tomstokes227@gmail.com>
2025-06-22 21:11:35 +0100

    tidy choices

--- src/to_do.adb
+++ src/to_do.adb
@@ -1,4 +1,5 @@
 with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Containers.Indefinite_Hashed_Maps;
 with Ada.Strings.Hash;
 procedure To_Do is
@@ -10,6 +11,7 @@ procedure To_Do is
          Equivalent_Keys => "=");
    use Tasks_Map;
    M : Map;
+   Choice : Integer;
 
    procedure All_Items is
    begin
@@ -20,19 +22,21 @@ procedure To_Do is
 
 begin
    M.Include("Wash Dishes", 10);
-   Put("1: View All 2: Add");
-   Put("Select Option: ");
-   Get(Choice);
-
-   case Choice is
-      when 1 =>
-         All_Items;
-      when 2 =>
-      when 3 =>
-         Put_Line ("Goodbye");
-         exit;
-      when others =>
-         Put("Not An Option");
-   end case;
+   loop
+      Put_Line("1: View All 2: Add 3: Exit");
+      Put("Select Option: ");
+      Get(Choice);
+      case Choice is
+         when 1 =>
+            All_Items;
+         when 2 =>
+            Put_Line ("test");
+         when 3 =>
+            Put_Line ("Goodbye");
+            exit;
+         when others =>
+            Put("Not An Option");
+      end case;
+   end loop;
          
 end To_Do;
