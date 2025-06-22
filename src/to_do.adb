4900d23ea5625bffda1f3a156762083fc38115a0
TS227 <tomstokes227@gmail.com>
2025-06-22 20:43:56 +0100

    implement hashmap of tasks

--- src/to_do.adb
+++ src/to_do.adb
@@ -1,7 +1,18 @@
 with Ada.Text_IO; use Ada.Text_IO;
-
-procedure ToDo is
+with Ada.Containers.Indefinite_Hashed_Maps;
+with Ada.Strings.Hash;
+procedure To_Do is
+   package Tasks_Map is new
+      Ada.Containers.Indefinite_Hashed_Maps
+         (Key_Type => String,
+         Element_Type => Integer,
+         Hash => Ada.Strings.Hash,
+         Equivalent_Keys => "=");
+   use Tasks_Map;
+   M : Map;
 begin
-   Put_Line ("Current Tasks");
-
-end ToDo;
+   M.Include("Wash Dishes", 10);
+   for Item in M.Iterate loop
+      Put_Line(Key (Item) & ": "& Integer'Image(M(Item)));
+   end loop;
+end To_Do;
